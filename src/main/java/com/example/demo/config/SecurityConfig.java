6664969ca923d6a69830b916ceac43fb59368f96
bala-amypo <bala@amypo.in>
2025-12-20 08:53:32 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 @Bean
@@ -13,15 +15,15 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 http
 .csrf(csrf -> csrf.disable())
-.formLogin(form -> form.disable())     
-.httpBasic(basic -> basic.disable()) 
+.formLogin(form -> form.disable())
+.httpBasic(basic -> basic.disable())
 .authorizeHttpRequests(auth -> auth
 .requestMatchers(
 "/auth/**",
 "/swagger-ui/**",
 "/v3/api-docs/**"
 ).permitAll()
-.anyRequest().authenticated()
+.anyRequest().permitAll()   
 );
 
 return http.build();
