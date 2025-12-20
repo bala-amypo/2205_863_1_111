2c784f3c61648389b9e9855c101d0ec832767976
bala-amypo <bala@amypo.in>
2025-12-20 10:38:37 +0000

    User commit

--- src/main/java/com/example/demo/model/HabitProfile.java
+++ src/main/java/com/example/demo/model/HabitProfile.java
@@ -4,15 +4,21 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-public class HabitProfile {
+public class HabitProfile {}
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 
 private Long studentId;
+
+private String sleepSchedule;     
 private Integer studyHoursPerDay;
-private LocalDateTime updatedAt = LocalDateTime.now();
+private String cleanlinessLevel;   
+private String noiseTolerance;      
+private String socialPreference;  
+
+private LocalDateTime updatedAt;
 
 public Long getId() {
 return id;
@@ -30,6 +36,14 @@ public void setStudentId(Long studentId) {
 this.studentId = studentId;
 }
 
+public String getSleepSchedule() {
+return sleepSchedule;
+}
+
+public void setSleepSchedule(String sleepSchedule) {
+this.sleepSchedule = sleepSchedule;
+}
+
 public Integer getStudyHoursPerDay() {
 return studyHoursPerDay;
 }
@@ -38,7 +52,35 @@ public void setStudyHoursPerDay(Integer studyHoursPerDay) {
 this.studyHoursPerDay = studyHoursPerDay;
 }
 
+public String getCleanlinessLevel() {
+return cleanlinessLevel;
+}
+
+public void setCleanlinessLevel(String cleanlinessLevel) {
+this.cleanlinessLevel = cleanlinessLevel;
+}
+
+public String getNoiseTolerance() {
+return noiseTolerance;
+}
+
+public void setNoiseTolerance(String noiseTolerance) {
+this.noiseTolerance = noiseTolerance;
+}
+
+public String getSocialPreference() {
+return socialPreference;
+}
+
+public void setSocialPreference(String socialPreference) {
+this.socialPreference = socialPreference;
+}
+
 public LocalDateTime getUpdatedAt() {
 return updatedAt;
+}
+
+public void setUpdatedAt(LocalDateTime updatedAt) {
+this.updatedAt = updatedAt;
 }
 }
