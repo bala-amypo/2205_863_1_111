c06ba881911a1a82d4975b451f9cb6466dfd7f62
bala-amypo <bala@amypo.in>
2025-12-23 15:35:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/CompatibilityScoreController.java
+++ src/main/java/com/example/demo/controller/CompatibilityScoreController.java
@@ -1,26 +1,21 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.CompatibilityScoreService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/compatibility")
-public class CompatibilityScoreController {}
+@RequestMapping("/api/compatibility")
+class CompatibilityScoreController {
 
 private final CompatibilityScoreService service;
 
-public CompatibilityScoreController(CompatibilityScoreService service) {
+CompatibilityScoreController(CompatibilityScoreService service) {
 this.service = service;
 }
 
-@GetMapping("/{studentA}/{studentB}")
-public int calculateCompatibility(
-@PathVariable Long studentA,
-@PathVariable Long studentB) {
-
-return service.calculateScore(studentA, studentB);
+@PostMapping
+int calculate(@RequestParam Long studentAId,
+@RequestParam Long studentBId) {
+return service.calculateScore(studentAId, studentBId);
 }
 }
