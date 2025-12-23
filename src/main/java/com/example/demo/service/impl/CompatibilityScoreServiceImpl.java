aca7f1cd7dee89ab38ced655cafffeaef35df06b
bala-amypo <bala@amypo.in>
2025-12-23 15:23:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CompatibilityScoreServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CompatibilityScoreServiceImpl.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
-public class CompatibilityScoreServiceImpl implements CompatibilityScoreService {}
+public class CompatibilityScoreServiceImpl implements CompatibilityScoreService {
 
 private final CompatibilityScoreRecordRepository scoreRepository;
 private final HabitProfileRepository habitRepository;
